101aea788900c2640f1ab895acc2553bf64b5ffd
段昊辰 <duanhaochen@126.com>
2018-09-26 00:53:21 +0800

    调整进度条信息

--- packages/milktea/src/utils.js
+++ packages/milktea/src/utils.js
@@ -2,6 +2,8 @@ export const isObject: Function = (d: any): boolean => typeof d === 'object' && 
 
 export const isArray: Function = (d: any): boolean => typeof d === 'object' && Object.prototype.toString.call(d) === '[object Array]';
 
-export function handleWebpackProgress(percentage: any, message: any, ...args: any): void{
-  console.info(percentage, message, ...args);
+export function handleWebpackProgress(percentage: number, ...args: any): void{
+  const pNumber: number = percentage.toFixed(2);
+
+  console.info(`${ pNumber * 100 }%`, ...args);
 }
