93d389b35ab07e8771f0d87b211f9b515d2ec465
段昊辰 <duanhaochen@126.com>
2018-09-26 20:48:29 +0800

    添加清除和编译脚本

---
+++ scripts/clean.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const rimraf = require('rimraf');
+const { dir, packageNames } = require('./utils');
+
+function clean(filePath){
+  return new Promise((resolve, reject)=>{
+    rimraf(filePath, function(){
+      resolve();
+    });
+  });
+}
+
+(async function(){
+  const queue = [];
+
+  for(let i = 0, j = packageNames.length; i < j; i++){
+    const p = path.join(dir, packageNames[i], 'lib');
+
+    queue.push(clean(p));
+  }
+
+  await Promise.all(queue);
+})();
---
+++ scripts/gulpfile.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const { dir, packageNames } = require('./utils');
+
+const configFile = path.join(__dirname, '../.babelrc');
+
+function createProject(names){
+  const src = path.join(dir, names, 'src/**/*.js');
+  const dist = path.join(dir, names, 'lib');
+
+  return function(){
+    return gulp.src(src)
+      .pipe(babel({
+        configFile
+      }))
+      .pipe(gulp.dest(dist));
+  }
+}
+
+/* 创建队列函数 */
+const queueFn = [];
+
+for(const item of packageNames){
+  const fn = createProject(item);
+
+  Object.defineProperty(fn, 'name', {
+    value: item
+  });
+  queueFn.push(fn);
+}
+
+gulp.task('default', gulp.parallel(...queueFn));
