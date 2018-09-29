d461dd2ca2f83d6af49b4f3c452bc113c5e32e40
duan602728596 <duanhaochen@126.com>
2018-09-29 16:37:41 +0800

    添加cli的测试用例

---
+++ packages/milktea-cli/test/build.js
@@ -0,0 +1,26 @@
+import child_process from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { expect } from 'chai';
+
+function run(): void{
+  return new Promise((resolve: Function, reject: Function): void=>{
+    const child: Object = child_process.spawn('node', ['../lib/cli', 'build'], {
+      cwd: __dirname,
+      env: {
+        NODE_ENV: 'production'
+      }
+    });
+
+    child.on('close', (code: string): void=>{
+      resolve();
+    });
+  });
+}
+
+describe('args: build', function(): void{
+  it('build', async function(): Promise<void>{
+    await run();
+    expect(fs.existsSync(path.join(__dirname, 'build/app.build.js'))).to.be.true;
+  });
+});
---
+++ packages/milktea-cli/test/test.js
@@ -0,0 +1,9 @@
+const register = require('@babel/register');
+const babelRegisterConfig = require('../../../utils/babelRegisterConfig');
+
+register(babelRegisterConfig);
+
+/* test */
+require('./dll');
+require('./build');
+require('./start');
