015da3d5bc63ce0b99aa569b81f2f6aa1b19da8f
段昊辰 <duanhaochen@126.com>
2018-10-05 23:33:52 +0800

    调整编译脚本

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -1,20 +1,18 @@
 const path = require('path');
 const gulp = require('gulp');
-const babel = require('gulp-babel');
+const typescript = require('gulp-typescript');
+const tsconfig = require('../tsconfig.json');
 const { dir, packageNames } = require('./config');
 
-const configFile = path.join(__dirname, '../.babelrc');
-
 function createProject(names){
-  const src = path.join(dir, names, 'src/**/*.js');
+  const src = path.join(dir, names, 'src/**/*.ts');
   const dist = path.join(dir, names, 'lib');
 
   return function(){
-    return gulp.src(src)
-      .pipe(babel({
-        configFile
-      }))
-      .pipe(gulp.dest(dist));
+    const result = gulp.src(src)
+      .pipe(typescript(tsconfig.compilerOptions));
+
+    return result.js.pipe(gulp.dest(dist));
   }
 }
 
