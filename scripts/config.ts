c9a9cac349892bf55b7da2fca1547e816621ce86
duan602728596 <duanhaochen@126.com>
2018-12-29 10:12:50 +0800

    修改脚本为typescript

---
+++ scripts/config.ts
@@ -0,0 +1,11 @@
+/* 通用配置 */
+import * as path from 'path';
+
+export const dir: string = path.join(__dirname, '../packages');
+
+export const packageNames: string[] = [
+  'milktea',
+  'milktea-cli',
+  'server',
+  'util-tools'
+];
---
+++ scripts/gulpfile.ts
@@ -0,0 +1,36 @@
+/* gulp配置文件，编译packages下的所有文件 */
+import * as path from 'path';
+import * as gulp from 'gulp';
+import * as typescript from 'gulp-typescript';
+import { ICompileStream } from 'gulp-typescript/release/project';
+import { Task } from 'undertaker';
+import ReadWriteStream = NodeJS.ReadWriteStream;
+import * as tsconfig from '../tsconfig.json';
+import { dir, packageNames } from './config';
+
+function createProject(names: string): Task{
+  const src: string = path.join(dir, names, 'src/**/*.ts');
+  const dist: string = path.join(dir, names, 'lib');
+
+  return function(): ReadWriteStream{
+    const result: ICompileStream = gulp.src(src)
+      .pipe(typescript(tsconfig.compilerOptions));
+
+    return result.js.pipe(gulp.dest(dist));
+  };
+}
+
+/* 创建队列函数 */
+const queueFn: Task[] = [];
+
+for(const item of packageNames){
+  const fn: Task = createProject(item);
+
+  Object.defineProperty(fn, 'name', {
+    value: item
+  });
+
+  queueFn.push(fn);
+}
+
+export default gulp.series(gulp.parallel(...queueFn));
