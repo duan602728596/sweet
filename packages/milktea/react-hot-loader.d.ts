97bb22c3ac345883156288a5e84ac644fb409b58
duan602728596 <duanhaochen@126.com>
2019-01-24 16:28:37 +0800

    导出root

--- packages/milktea/react-hot-loader.d.ts
+++ packages/milktea/react-hot-loader.d.ts
@@ -1,1 +1,4 @@
+import * as root from 'react-hot-loader/root';
+
 export * from 'react-hot-loader';
+export { root };
--- packages/milktea/react-hot-loader.js
+++ packages/milktea/react-hot-loader.js
@@ -1,1 +1,2 @@
 module.exports = require('react-hot-loader');
+module.exports.root = require('react-hot-loader/root');
