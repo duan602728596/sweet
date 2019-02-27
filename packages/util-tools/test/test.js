c78be628fd8095dc6c3c02923326e5041b4bd0a1
duan602728596 <duanhaochen@126.com>
2019-02-27 13:48:33 +0800

    test: 调整测试用例

--- packages/util-tools/test/test.js
+++
@@ -1,8 +0,0 @@
-const register = require('@babel/register');
-const babelRegisterConfig = require('../../../utils/babelRegisterConfig');
-
-register(babelRegisterConfig);
-
-/* test */
-require('./update');
-require('./image2webp/image2webp');
---
+++ packages/util-tools/tests/test.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+const register = require('@babel/register');
+const babelRegisterConfig = require('../../../utils/babelRegisterConfig');
+
+register(babelRegisterConfig);
+
+/* test */
+require('./update');
+require('./image2webp/image2webp');
