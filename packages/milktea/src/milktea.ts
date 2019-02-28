f2cbd13d5177f40b0366403ebf74cedd3e2be234
duan602728596 <duanhaochen@126.com>
2019-02-28 09:34:16 +0800

    refactor: 优化代码，提取方法

--- packages/milktea/src/milktea.ts
+++ packages/milktea/src/milktea.ts
@@ -1,64 +1,16 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import * as process from 'process';
-import * as cosmiconfig from 'cosmiconfig';
 import * as webpack from 'webpack';
 import webpackConfig from './config';
 import serverConfig from './server';
 import webpackDll from './dll';
 import { isObject } from './utils/utils';
-import jsRegisterLoader from './utils/jsRegisterLoader';
 import { SweetConfig, SweetOptions } from './utils/types';
+import getSweetConfigFile from './utils/getSweetConfigFile';
 
 /* 基础配置 */
 const sweetOptions: SweetOptions = {
   basicPath: process.cwd() // 主目录
 };
-
-/* cosmiconfig */
-const moduleName: string = 'sweet';
-const explorer: { searchSync: Function } = cosmiconfig(moduleName, {
-  searchPlaces: [
-    `${ moduleName }.config.js`,
-    `.${ moduleName }rc.js`
-  ],
-  loaders: {
-    '.js': jsRegisterLoader
-  },
-  stopDir: sweetOptions.basicPath
-});
-
-/* 获取配置文件 */
-const errorMsg: string = 'Please configure the .sweetrc.js or sweet.config.js file first.';
-
-function getSweetConfigFile(configFile?: string): SweetConfig {
-  if (typeof configFile === 'string') {
-    // 加载其他的配置文件
-    let sweetConfigFile: string;
-
-    if (path.isAbsolute(configFile)) {
-      sweetConfigFile = configFile;
-    } else {
-      sweetConfigFile = path.join(sweetOptions.basicPath, configFile);
-    }
-
-    if (fs.existsSync(sweetConfigFile)) {
-      // 加载es6+环境
-      return jsRegisterLoader(sweetConfigFile);
-    } else {
-      throw new Error(errorMsg);
-    }
-  } else {
-    // 加载默认的配置文件
-    const searchResult: { config: SweetConfig; filepath: string } | null = explorer.searchSync();
-
-    if (searchResult === null) {
-      throw new Error(errorMsg);
-    } else {
-      return searchResult.config;
-    }
-  }
-}
 
 /* webpack的回调函数 */
 export function callback(err: Error, stats: webpack.Stats): void {
@@ -76,11 +28,11 @@ export function config(sweetConfig: SweetConfig | string | null, mode: string): 
   let config: SweetConfig | null;
 
   if (typeof sweetConfig === 'string') {
-    config = getSweetConfigFile(sweetConfig);
+    config = getSweetConfigFile(sweetOptions, sweetConfig);
   } else if (isObject(sweetConfig)) {
     config = sweetConfig;
   } else {
-    config = getSweetConfigFile();
+    config = getSweetConfigFile(sweetOptions);
   }
 
   if (config && mode) {
@@ -99,11 +51,11 @@ export function serverRenderConfig(sweetConfig: SweetConfig | string | null, mod
   let config: SweetConfig | null;
 
   if (typeof sweetConfig === 'string') {
-    config = getSweetConfigFile(sweetConfig);
+    config = getSweetConfigFile(sweetOptions, sweetConfig);
   } else if (isObject(sweetConfig)) {
     config = sweetConfig;
   } else {
-    config = getSweetConfigFile();
+    config = getSweetConfigFile(sweetOptions);
   }
 
   if (config && mode) {
@@ -121,11 +73,11 @@ export function dll(sweetConfig: SweetConfig | string | null): object {
   let config: SweetConfig | null;
 
   if (typeof sweetConfig === 'string') {
-    config = getSweetConfigFile(sweetConfig);
+    config = getSweetConfigFile(sweetOptions, sweetConfig);
   } else if (isObject(sweetConfig)) {
     config = sweetConfig;
   } else {
-    config = getSweetConfigFile();
+    config = getSweetConfigFile(sweetOptions);
   }
 
   return webpackDll(config, sweetOptions);
---
+++ packages/milktea/src/utils/getSweetConfigFile.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as cosmiconfig from 'cosmiconfig';
+import { SweetConfig, SweetOptions } from './types';
+import { registerConfig, requireModule } from './utils';
+
+/* cosmiconfig的js加载器 */
+function jsRegisterLoader(filepath: string, content?: string): any {
+  const register: Function = requireModule('@babel/register');
+
+  register(registerConfig);
+
+  return requireModule(filepath);
+}
+
+/* 获取配置文件 */
+function getSweetConfigFile(sweetOptions: SweetOptions, configFile?: string): SweetConfig {
+  const moduleName: string = 'sweet';
+  const explorer: { searchSync: Function } = cosmiconfig(moduleName, {
+    searchPlaces: [
+      `${ moduleName }.config.js`,
+      `.${ moduleName }rc.js`
+    ],
+    loaders: {
+      '.js': jsRegisterLoader
+    },
+    stopDir: sweetOptions.basicPath
+  });
+  const errorMsg: string = 'Please configure the .sweetrc.js or sweet.config.js file first.';
+
+  if (typeof configFile === 'string') {
+    // 加载其他的配置文件
+    let sweetConfigFile: string;
+
+    if (path.isAbsolute(configFile)) {
+      sweetConfigFile = configFile;
+    } else {
+      sweetConfigFile = path.join(sweetOptions.basicPath, configFile);
+    }
+
+    if (fs.existsSync(sweetConfigFile)) {
+      // 加载es6+环境
+      return jsRegisterLoader(sweetConfigFile);
+    } else {
+      throw new Error(errorMsg);
+    }
+  } else {
+    // 加载默认的配置文件
+    const searchResult: { config: SweetConfig; filepath: string } | null = explorer.searchSync();
+
+    if (searchResult === null) {
+      throw new Error(errorMsg);
+    } else {
+      return searchResult.config;
+    }
+  }
+}
+
+export default getSweetConfigFile;
