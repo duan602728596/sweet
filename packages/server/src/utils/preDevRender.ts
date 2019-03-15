abf90e57196ee4039be31595f84a3527c8b59401
段昊辰 <duanhaochen@126.com>
2019-03-16 01:38:24 +0800

    fix: 修复模块加载错误

--- packages/server/src/utils/preDevRender.ts
+++ packages/server/src/utils/preDevRender.ts
@@ -13,15 +13,15 @@ async function preRender(
   cleanRequireCache(serverRenderFile);
 
   const basicPath: string = sweetOptions.basicPath;
-  const filesMap: Map<string, string> = await getControllersFiles(basicPath);
+  const controllersMap: Map<string, string> = await getControllersFiles(basicPath);
   const folderPathFile: string = `${ folderPathAnalyze(file) }.js`; // 格式化为：path/to/file.js
   const formatFile: string = `${ filePathAnalyze(file) }.js`; // 格式化为：path.to.file.js
   let data: any = {};
 
   // 查找对应的controller文件
-  if (filesMap.has(folderPathFile)) {
+  if (controllersMap.has(folderPathFile)) {
     // 查找文件夹
-    const file: string | undefined = filesMap.get(folderPathFile);
+    const file: string | undefined = controllersMap.get(folderPathFile);
 
     if (file) {
       cleanRequireCache(file);
@@ -30,20 +30,20 @@ async function preRender(
 
       data = await module(ctx, sweetOptions);
     }
-  } else if (filesMap.has(formatFile)) {
+  } else if (controllersMap.has(formatFile)) {
     // 查找文件
-    const file: string | undefined = filesMap.get(formatFile);
+    const file: string | undefined = controllersMap.get(formatFile);
 
     if (file) {
-      cleanRequireCache(formatFile);
+      cleanRequireCache(file);
 
       const module: Function = requireModule(file);
 
       data = await module(ctx, sweetOptions);
     }
-  } else if (filesMap.has('default.js')) {
+  } else if (controllersMap.has('default.js')) {
     // 查找默认文件
-    const defaultFile: string | undefined = filesMap.get('default.js');
+    const defaultFile: string | undefined = controllersMap.get('default.js');
 
     if (defaultFile) {
       cleanRequireCache(defaultFile);
