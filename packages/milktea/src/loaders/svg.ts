55bcd4bf1754e26abf3b121aa1cbe23c399af827
段昊辰 <duanhaochen@126.com>
2019-03-19 23:45:34 +0800

    feat: svg在react和vue下使用不同的loader加载

--- packages/milktea/src/loaders/svg.ts
+++ packages/milktea/src/loaders/svg.ts
@@ -7,18 +7,57 @@ export default function(sweetConfig: SweetConfig, config: Config): void {
    * mode { string }: 开发模式还是生产模式
    * serverRender { boolean }: 开启服务器端渲染
    */
-  const { mode, serverRender }: SweetConfig = sweetConfig;
+  const { mode, serverRender, frame }: SweetConfig = sweetConfig;
   const isDevelopment: boolean = mode === 'development';
   const filename: string = isDevelopment ? '[name].[hash:5].[ext]' : '[hash:5].[ext]';
 
+  // 当不是react或vue时
   config
-    .module
-    .rule('svg')
-    .test(/\.svg$/)
-    .use('file-loader')
-    .loader('file-loader')
-    .options( {
-      name: filename,
-      emitFile: !serverRender
-    });
+    .when(!(frame === 'react' || frame === 'vue'),
+      (config: Config): void => {
+        config
+          .module
+          .rule('svg')
+          .test(/\.svg$/)
+          .use('file-loader')
+          .loader('file-loader')
+          .options( {
+            name: filename,
+            emitFile: !serverRender
+          });
+      }
+    );
+
+  // 当环境是react时
+  config
+    .when(frame === 'react',
+      (config: Config): void => {
+        config
+          .module
+          .rule('svg')
+          .test(/\.svg(\?v=\d+\.\d+\.\d+)?$/)
+          .use('babel-loader')
+          .loader('babel-loader')
+          .end()
+          .use('@svgr/webpack')
+          .loader('@svgr/webpack')
+          .options({
+            babel: false,
+            icon: true
+          });
+      }
+    );
+
+  // 当环境是vue时
+  config
+    .when(frame === 'vue',
+      (config: Config): void => {
+        config
+          .module
+          .rule('svg')
+          .test(/\.svg(\?v=\d+\.\d+\.\d+)?$/)
+          .use('vue-svg-loader')
+          .loader('vue-svg-loader');
+      }
+    );
 }
