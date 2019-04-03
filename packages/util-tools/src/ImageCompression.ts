21cce400afff3bf42c18b02d69dfbc35feba47d0
duan602728596 <duanhaochen@126.com>
2019-04-03 10:28:32 +0800

    feat: 添加图片压缩方法

---
+++ packages/util-tools/src/ImageCompression.ts
@@ -0,0 +1,22 @@
+import * as path from 'path';
+import * as imageMin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import * as imageminJpegoptim from 'imagemin-jpegoptim';
+import * as imageminGifsicle from 'imagemin-gifsicle';
+
+/**
+ * 批量压缩图片
+ * @param { string } entry: 入口文件夹
+ * @param { string } output: 输出文件夹
+ */
+async function imageCompression(entry: string, output: string): Promise<void> {
+  await imageMin([path.join(entry, '*.{jpg,png,gif}')], output, {
+    use: [
+      imageminPngquant(),
+      imageminJpegoptim(),
+      imageminGifsicle()
+    ]
+  });
+}
+
+export default imageCompression;
