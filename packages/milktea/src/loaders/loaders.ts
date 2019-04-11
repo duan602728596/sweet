407a327bcb2952b70e8aa7c7772a359ba285e1c3
duan602728596 <duanhaochen@126.com>
2019-04-11 12:36:09 +0800

    refactor: 修改输出文件名称

--- packages/milktea/src/loaders/loaders.ts
+++ packages/milktea/src/loaders/loaders.ts
@@ -153,7 +153,7 @@ export default function(sweetConfig: SweetConfig, sweetOptions: SweetOptions, co
     .use('file-loader')
     .loader('file-loader')
     .options({
-      name: isDevelopment ? '[name].[ext]' : '[hash:5].[ext]',
+      name: isDevelopment ? '[name].[hash:5].[ext]' : '[hash:5].[ext]',
       outputPath: 'script/'
     });
 }
