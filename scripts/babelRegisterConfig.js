992c8f0d1a54c0cf9b6a54074d94d485fab038f9
duan602728596 <duanhaochen@126.com>
2019-04-28 10:20:29 +0800

    refactor: 调整测试环境的配置

--- scripts/babelRegisterConfig.js
+++ scripts/babelRegisterConfig.js
@@ -12,15 +12,5 @@ module.exports = {
       }
     ]
   ],
-  plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-do-expressions',
-    '@babel/plugin-proposal-optional-catch-binding',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-syntax-bigint'
-  ],
   cache: false
 };
