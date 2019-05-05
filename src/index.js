bdf61c934908251e06bc2f74c8ba3227c00d249e
duan602728596 <duanhaochen@126.com>
2019-05-05 10:35:09 +0800

    refactor: 调整render的事件

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { hydrate } from 'react-dom';
 import App from './App';
 
 /* app */
-render(
+hydrate(
   <App />,
   document.getElementById('app')
 );
