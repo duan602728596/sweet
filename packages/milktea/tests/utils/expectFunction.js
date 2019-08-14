9d7b3a64b32a6c807fbce41293f0efca9a18e569
段昊辰 <duanhaochen@126.com>
2019-08-14 22:52:54 +0800

    test: 调整测试用例

--- packages/milktea/tests/utils/expectFunction.js
+++ packages/milktea/tests/utils/expectFunction.js
@@ -21,10 +21,11 @@ export function expectModule(config, length) {
 export function expectDevPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(3);
+    expect(config.plugins).to.have.lengthOf(4);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
-    expect(config.plugins[1] instanceof HardSourceWebpackPlugin).to.be.true;
-    expect(config.plugins[2] instanceof HtmlWebpackPlugin).to.be.true;
+    expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
+    expect(config.plugins[2] instanceof HardSourceWebpackPlugin).to.be.true;
+    expect(config.plugins[3] instanceof HtmlWebpackPlugin).to.be.true;
   };
 }
 
@@ -32,9 +33,10 @@ export function expectDevPlugins(config) {
 export function expectDevServerPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(2);
+    expect(config.plugins).to.have.lengthOf(3);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
-    expect(config.plugins[1] instanceof HardSourceWebpackPlugin).to.be.true;
+    expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
+    expect(config.plugins[2] instanceof HardSourceWebpackPlugin).to.be.true;
   };
 }
 
@@ -42,14 +44,15 @@ export function expectDevServerPlugins(config) {
 export function expectProPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(7);
+    expect(config.plugins).to.have.lengthOf(8);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
-    expect(config.plugins[1] instanceof MiniCssExtractPlugin).to.be.true;
-    expect(config.plugins[2] instanceof OptimizeCssAssets).to.be.true;
-    expect(config.plugins[3] instanceof ImageMinWebpackPlugin).to.be.true;
-    expect(config.plugins[4] instanceof webpack.ProgressPlugin).to.be.true;
-    expect(config.plugins[5] instanceof HtmlWebpackPlugin).to.be.true;
-    expect(config.plugins[6] instanceof VueLoaderPlugin).to.be.true;
+    expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
+    expect(config.plugins[2] instanceof MiniCssExtractPlugin).to.be.true;
+    expect(config.plugins[3] instanceof OptimizeCssAssets).to.be.true;
+    expect(config.plugins[4] instanceof ImageMinWebpackPlugin).to.be.true;
+    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[6] instanceof HtmlWebpackPlugin).to.be.true;
+    expect(config.plugins[7] instanceof VueLoaderPlugin).to.be.true;
   };
 }
 
@@ -57,13 +60,14 @@ export function expectProPlugins(config) {
 export function expectProServerPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(6);
+    expect(config.plugins).to.have.lengthOf(7);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
-    expect(config.plugins[1] instanceof MiniCssExtractPlugin).to.be.true;
-    expect(config.plugins[2] instanceof OptimizeCssAssets).to.be.true;
-    expect(config.plugins[3] instanceof ImageMinWebpackPlugin).to.be.true;
-    expect(config.plugins[4] instanceof webpack.ProgressPlugin).to.be.true;
-    expect(config.plugins[5] instanceof VueLoaderPlugin).to.be.true;
+    expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
+    expect(config.plugins[2] instanceof MiniCssExtractPlugin).to.be.true;
+    expect(config.plugins[3] instanceof OptimizeCssAssets).to.be.true;
+    expect(config.plugins[4] instanceof ImageMinWebpackPlugin).to.be.true;
+    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[6] instanceof VueLoaderPlugin).to.be.true;
   };
 }
 
