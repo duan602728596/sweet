77fe6ce425fa86a9bf1b337df6b242acef6a3956
段昊辰 <duanhaochen@126.com>
2019-09-01 18:19:32 +0800

    fix: 修复vue下svg的loader引入

--- packages/milktea/src/loaders/image.ts
+++ packages/milktea/src/loaders/image.ts
@@ -7,7 +7,7 @@ export default function(sweetConfig: SweetConfig, config: Config): void {
   config
     .module
     .rule('image')
-    .test(/^.*\.(jpe?g|png|gif|webp|svg)$/)
+    .test(/^.*\.(jpe?g|png|gif|webp)|(?<!component)\.svg$/)
     .use('url-loader')
     .loader('url-loader')
     .options(createImageConfig(sweetConfig));
