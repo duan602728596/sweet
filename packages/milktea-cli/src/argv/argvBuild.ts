ae74ef2dd8d0fbbf3ccecd94336fdf8d201e0f9d
段昊辰 <duanhaochen@126.com>
2019-10-12 00:32:21 +0800

    fix: 避免输出的log打断进度条

--- packages/milktea-cli/src/argv/argvBuild.ts
+++ packages/milktea-cli/src/argv/argvBuild.ts
@@ -13,12 +13,15 @@ function argvBuild(argv: Argv): void {
 
   if (!_.isNil(argv.serverRender)) {
     // 正常编译完毕后，编译ssr需要的文件
-    compiler.hooks.done.tap('build-ssr', function(): void {
+    compiler.hooks.done.tapAsync('sweet-milktea-build', function(): void {
       const serverRenderCompiler: Compiler = webpack(
         milktea.serverRenderConfig(argv.config, 'production', argv.webpackLog)
       );
 
-      serverRenderCompiler.run(!argv.webpackLog || argv.webpackLog === 'progress' ? milktea.callbackOnlyError : milktea.callback);
+      // 避免输出的log打断进度条
+      serverRenderCompiler.run(!argv.webpackLog || argv.webpackLog === 'progress'
+        ? milktea.callbackOnlyError
+        : milktea.callback);
     });
   }
 
