924f4f5569d4b6929670692cca2eed79f92dcee8
段昊辰 <duanhaochen@126.com>
2019-10-18 00:03:47 +0800

    perf: 添加文件的重定向

--- packages/server/src/alias.ts
+++ packages/server/src/alias.ts
@@ -3,3 +3,6 @@ import * as moduleAlias from 'module-alias';
 
 // http-proxy模块不支持http2
 moduleAlias.addAlias('http-proxy', '@bbkkbkk/http-proxy');
+
+// webpack5 && webpack-hot-client/lib/compiler.js
+moduleAlias.addAlias('webpack/lib/ParserHelpers', 'webpack/lib/JavascriptParserHelpers');
