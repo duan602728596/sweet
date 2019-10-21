220c3c986f72c76b99576c9e8ffd243e3fc98fc9
duan602728596 <duanhaochen@126.com>
2019-10-21 12:38:15 +0800

    feat: 添加babel的管道函数插件

--- packages/server/src/utils/babelRegister.ts
+++ packages/server/src/utils/babelRegister.ts
@@ -34,6 +34,7 @@ function createRegisterConfig(): RegisterConfig {
       '@babel/plugin-proposal-numeric-separator',              // 1_000_000 语法
       '@babel/plugin-proposal-optional-catch-binding',         // try {} catch {} 语法
       '@babel/plugin-proposal-optional-chaining',              // x?.y 语法
+      ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }], // 管道函数
       '@babel/plugin-syntax-bigint',                           // BigInt数据类型
       '@babel/plugin-syntax-dynamic-import'                    // import() 语法
     ],
