0e81b63f6e52b9c74386a0297f41837a4193f1cc
段昊辰 <duanhaochen@126.com>
2019-12-25 09:32:11 +0800

    perf: proxy和api的创建进行容错处理

--- packages/server/src/utils/createApi.ts
+++ packages/server/src/utils/createApi.ts
@@ -4,22 +4,29 @@ import * as Router from '@koa/router';
 import { defaultApiPath, deleteCacheAndRequireModule, requireModule } from './utils';
 import { SweetOptions } from './types';
 
-/* 本地api，本地api无法清除缓存 */
+/**
+ * 本地api
+ * TODO: 本地api无法清除缓存
+ */
 async function createApi(sweetOptions: SweetOptions, router: Router, app: Koa, isDevelopment: boolean): Promise<void> {
-  const defaultApi: string = defaultApiPath(sweetOptions.basicPath);
+  try {
+    const defaultApi: string = defaultApiPath(sweetOptions.basicPath);
 
-  if (sweetOptions.apiFile && fs.existsSync(sweetOptions.apiFile)) {
-    const routers: Function = isDevelopment
-      ? deleteCacheAndRequireModule(sweetOptions.apiFile)
-      : requireModule(sweetOptions.apiFile);
+    if (sweetOptions.apiFile && fs.existsSync(sweetOptions.apiFile)) {
+      const routers: Function = isDevelopment
+        ? deleteCacheAndRequireModule(sweetOptions.apiFile)
+        : requireModule(sweetOptions.apiFile);
 
-    await routers(router, sweetOptions, app);
-  } else if (fs.existsSync(defaultApi)) {
-    const routers: Function = isDevelopment
-      ? deleteCacheAndRequireModule(defaultApi)
-      : requireModule(defaultApi);
+      await routers(router, sweetOptions, app);
+    } else if (fs.existsSync(defaultApi)) {
+      const routers: Function = isDevelopment
+        ? deleteCacheAndRequireModule(defaultApi)
+        : requireModule(defaultApi);
 
-    await routers(router, sweetOptions, app);
+      await routers(router, sweetOptions, app);
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
--- packages/server/src/utils/createProxy.ts
+++ packages/server/src/utils/createProxy.ts
@@ -25,34 +25,41 @@ function addMiddleware(app: Koa, proxyConfig: ProxyConfig, isDevelopment: boolea
   }
 }
 
-/* 本地代理，本地代理无法清除缓存 */
+/**
+ * 本地代理
+ * TODO: 本地代理无法清除缓存
+ */
 async function createProxy(sweetOptions: SweetOptions, app: Koa, isDevelopment: boolean, env?: string): Promise<void> {
-  const defaultProxy: { js: string; json: string } = defaultProxyPath(sweetOptions.basicPath);
+  try {
+    const defaultProxy: { js: string; json: string } = defaultProxyPath(sweetOptions.basicPath);
 
-  if (sweetOptions.proxyFile && fs.existsSync(sweetOptions.proxyFile)) {
-    const module: any = requireModule(sweetOptions.proxyFile);
+    if (sweetOptions.proxyFile && fs.existsSync(sweetOptions.proxyFile)) {
+      const module: any = requireModule(sweetOptions.proxyFile);
 
-    if (_.isPlainObject(module)) {
-      addMiddleware(app, module, isDevelopment, env);
-    } else if (typeof module === 'function') {
-      const proxyConfig: ProxyConfig = await module(sweetOptions, app);
+      if (_.isPlainObject(module)) {
+        await addMiddleware(app, module, isDevelopment, env);
+      } else if (typeof module === 'function') {
+        const proxyConfig: ProxyConfig = await module(sweetOptions, app);
 
-      addMiddleware(app, proxyConfig, isDevelopment, env);
+        await addMiddleware(app, proxyConfig, isDevelopment, env);
+      }
+    } else if (fs.existsSync(defaultProxy.js)) {
+      const module: any = requireModule(defaultProxy.js);
+
+      if (_.isPlainObject(module)) {
+        await addMiddleware(app, module, isDevelopment, env);
+      } else if (typeof module === 'function') {
+        const proxyConfig: ProxyConfig = await module(sweetOptions, app);
+
+        await addMiddleware(app, proxyConfig, isDevelopment, env);
+      }
+    } else if (fs.existsSync(defaultProxy.json)) {
+      const module: ProxyConfig = requireModule(defaultProxy.json);
+
+      await addMiddleware(app, module, isDevelopment, env);
     }
-  } else if (fs.existsSync(defaultProxy.js)) {
-    const module: any = requireModule(defaultProxy.js);
-
-    if (_.isPlainObject(module)) {
-      addMiddleware(app, module, isDevelopment, env);
-    } else if (typeof module === 'function') {
-      const proxyConfig: ProxyConfig = await module(sweetOptions, app);
-
-      addMiddleware(app, proxyConfig, isDevelopment, env);
-    }
-  } else if (fs.existsSync(defaultProxy.json)) {
-    const module: ProxyConfig = requireModule(defaultProxy.json);
-
-    addMiddleware(app, module, isDevelopment, env);
+  } catch (err) {
+    console.error(err);
   }
 }
 
