c09f0b7fd8680489080af4292a670be8d6626b29
段昊辰 <duanhaochen@126.com>
2020-01-17 21:48:10 +0800

    test: 修改测试用例

--- packages/milktea-cli/tests/sweet.config.js
+++ packages/milktea-cli/tests/sweet.config.js
@@ -1,20 +1,22 @@
 import path from 'path';
 
-export default {
-  frame: 'react',
-  dll: ['react'],
-  entry: {
-    index: [path.join(__dirname, 'src/index.js')]
-  },
-  loaders: {
-    js: {
-      test: /.*\.jsx?$/,
-      use: ['babel-loader'],
-      include: /src/
+export default function(info) {
+  return {
+    frame: 'react',
+    dll: ['react'],
+    entry: {
+      index: [path.join(__dirname, 'src/index.js')]
+    },
+    loaders: {
+      js: {
+        test: /.*\.jsx?$/,
+        use: ['babel-loader'],
+        include: /src/
+      }
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js'
     }
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js'
-  }
-};
+  };
+}
