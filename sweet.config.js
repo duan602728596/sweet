be8085bb4207b2bf593901c09f93a0334a2223c9
段昊辰 <duanhaochen@126.com>
2020-01-17 22:14:16 +0800

    build: 修改配置

--- sweet.config.js
+++ sweet.config.js
@@ -1,40 +1,42 @@
 import path from 'path';
 
-export default {
-  frame: 'vue',
-  dll: [
-    'vue',
-    'vue-router',
-    'vuex',
-    'vue-class-component',
-    'vue-meta',
-    'classnames'
-  ],
-  entry: {
-    index: [path.join(__dirname, 'src/index.js')],
-    other: [path.join(__dirname, 'src/other.js')]
-  },
-  serverRender: true,
-  serverEntry: {
-    server: [path.join(__dirname, 'src/server.js')]
-  },
-  js: {
-    plugins: [['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true }]],
-    exclude: /node_modules/
-  },
-  sass: {
-    include: /src/
-  },
-  css: {
-    modifyVars: {
-      // https://github.com/vueComponent/ant-design-vue/blob/master/components/style/themes/default.less
-      '@primary-color': '#58b957'
+export default function(info) {
+  return {
+    frame: 'vue',
+    dll: [
+      'vue',
+      'vue-router',
+      'vuex',
+      'vue-class-component',
+      'vue-meta',
+      'classnames'
+    ],
+    entry: {
+      index: [path.join(__dirname, 'src/index.js')],
+      other: [path.join(__dirname, 'src/other.js')]
     },
-    include: /node_modules[\\/]ant-design-vue/
-  },
-  html: [
-    { template: path.join(__dirname, 'src/index.pug'), excludeChunks: ['other'] },
-    { template: path.join(__dirname, 'src/other.pug'), excludeChunks: ['index'] }
-  ],
-  filesMap: true
-};
+    serverRender: true,
+    serverEntry: {
+      server: [path.join(__dirname, 'src/server.js')]
+    },
+    js: {
+      plugins: [['import', { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true }]],
+      exclude: /node_modules/
+    },
+    sass: {
+      include: /src/
+    },
+    css: {
+      modifyVars: {
+        // https://github.com/vueComponent/ant-design-vue/blob/master/components/style/themes/default.less
+        '@primary-color': '#58b957'
+      },
+      include: /node_modules[\\/]ant-design-vue/
+    },
+    html: [
+      { template: path.join(__dirname, 'src/index.pug'), excludeChunks: ['other'] },
+      { template: path.join(__dirname, 'src/other.pug'), excludeChunks: ['index'] }
+    ],
+    filesMap: true
+  };
+}
