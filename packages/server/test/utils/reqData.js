726a905a0a1fb7cdb00b959a709fbdd0e3812c0d
段昊辰 <duanhaochen@126.com>
2020-02-12 13:37:19 +0800

    test: 修改测试用例，使用node-fetch替换request

--- packages/server/test/utils/reqData.js
+++ packages/server/test/utils/reqData.js
@@ -1,46 +1,29 @@
-import request from 'request';
+import fetch from 'node-fetch';
 
-export function get(uri, json = false) {
-  return new Promise((resolve, reject) => {
-    request({
-      uri,
-      method: 'GET',
-      timeout: 15000,
-      json
-    }, function(err, res, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          statusCode: res.statusCode,
-          data
-        });
-      }
-    });
-  }).catch((err) => {
-    console.error(err);
-  });
+export async function get(uri, json = false) {
+  const options = {
+    method: 'GET',
+    timeout: 15000
+  };
+
+  if (json) {
+    options.headers = { 'Content-Type': 'application/json' };
+  }
+
+  const res = await fetch(uri, options);
+  const data = json ? await res.json() : await res.text();
+
+  return { statusCode: res.status, data };
 }
 
-export function post(uri, body) {
-  return new Promise((resolve, reject) => {
-    request({
-      uri,
-      method: 'POST',
-      timeout: 15000,
-      json: true,
-      body
-    }, function(err, res, data) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          statusCode: res.statusCode,
-          data
-        });
-      }
-    });
-  }).catch((err) => {
-    console.error(err);
+export async function post(uri, body) {
+  const res = await fetch(uri, {
+    method: 'POST',
+    timeout: 15000,
+    body: JSON.stringify(body),
+    headers: { 'Content-Type': 'application/json' }
   });
+  const data = await res.json();
+
+  return { statusCode: res.status, data };
 }
