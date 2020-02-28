5a1385f491d0573ccdc2c385e082b04fd73968a2
段昊辰 <duanhaochen@126.com>
2020-02-28 13:47:18 +0800

    refactor: 升级webpack-dev-middleware的版本

--- packages/server/src/devServer/middleware.ts
+++ packages/server/src/devServer/middleware.ts
@@ -1,4 +1,3 @@
-import * as path from 'path';
 import * as Koa from 'koa';
 import * as Router from '@koa/router';
 import * as compress from '@bbkkbkk/koa-compress';
@@ -6,24 +5,20 @@ import * as body from 'koa-body';
 import * as connect from 'koa-connect';
 import { Compiler } from 'webpack';
 import * as hotMiddleware from 'webpack-hot-middleware';
-import { fs as iFs, IFs } from 'memfs';
 import koaDevMiddleware from './koaDevMiddleware';
 import { webpackHmrPath } from '../utils/utils';
 import { WebpackLog } from '../utils/types';
 
-/* 使用memfs替换webpack的文件系统 */
-interface DevMiddlewareFs extends IFs {
-  join: (...paths: string[]) => string;
-}
-
-const devMiddlewareFs: DevMiddlewareFs = Object.assign(iFs, {
-  join: path.join
-});
-
 /**
  * 创建中间件
  */
-function middleware(app: Koa, router: Router, compiler: Compiler | undefined, webpackLog: WebpackLog | undefined, env: string | undefined): void {
+function middleware(
+  app: Koa,
+  router: Router,
+  compiler: Compiler | undefined,
+  webpackLog: WebpackLog | undefined,
+  env: string | undefined
+): void {
   /* 文件压缩 */
   app.use(compress({
     useBrCompress: false // 关闭brotli压缩
@@ -41,31 +36,19 @@ function middleware(app: Koa, router: Router, compiler: Compiler | undefined, we
     const hotMiddlewareConfig: { [key: string]: any } = {
       path: webpackHmrPath,
       log: false
-    },
-      devMiddlewareConfig: { [key: string]: any } = {
-        publicPath: compiler?.options?.output?.publicPath ?? '/',
-        serverSideRender: true,
-        fs: devMiddlewareFs
-      };
+    };
+    const devMiddlewareConfig: { [key: string]: any } = {
+      serverSideRender: true
+    };
 
     // webpack log
     if (!webpackLog || webpackLog === 'progress') {
-      devMiddlewareConfig.logLevel = 'warn';
-      devMiddlewareConfig.stats = {
-        colors: true,
-        assets: false,
-        entrypoints: false,
-        builtAt: false,
-        hash: false,
-        modules: false,
-        version: false,
-        timings: false
-      };
+      devMiddlewareConfig.infrastructureLogging.logLevel = 'warn';
     }
 
     // 测试配置
     if (env === 'test') {
-      devMiddlewareConfig.logLevel = 'silent';
+      devMiddlewareConfig.infrastructureLogging.logLevel = 'silent';
     }
 
     app.use(connect(
