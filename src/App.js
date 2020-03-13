e59632031bce9fd657647795d71f79474200590e
段昊辰 <duanhaochen@126.com>
2020-03-14 00:47:58 +0800

    refactor: 移除无用模块

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigProvider } from 'antd';
