293bb8fa9b9fea00a620016649ed0cba08ac1138
段昊辰 <duanhaochen@126.com>
2020-04-08 09:34:00 +0800

    refactor: 调整路由

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,19 +1,23 @@
 import React, { Fragment } from 'react';
+import { useRoutes } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Main from '../../layouts/Main';
 import Content from '../../layouts/Content';
 import Welcome from './Welcome';
 
 function Index(props) {
+  const routes = useRoutes([
+    { path: '/', element: <Welcome /> },
+    { path: 'Index', element: <Welcome /> }
+  ]);
+
   return (
     <Fragment>
       <Helmet>
         <title>Webpack App</title>
       </Helmet>
       <Main>
-        <Content>
-          <Welcome />
-        </Content>
+        <Content>{ routes }</Content>
       </Main>
     </Fragment>
   );
--- src/router/Routers.js
+++ src/router/Routers.js
@@ -8,8 +8,7 @@ const ListBundle = asyncModule(() => import(/* webpackChunkName: 'list' */ '../p
 
 function Routers(props) {
   const routes = useRoutes([
-    { path: '/', element: <Index /> },
-    { path: 'Index', element: <Index /> },
+    { path: '//*', element: <Index /> },
     { path: 'Second/*', element: <SecondBundle /> },
     { path: 'List/*', element: <ListBundle /> }
   ]);
