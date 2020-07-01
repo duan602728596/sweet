4f427a7a38f1f65fac38816370611ee10423509a
段昊辰 <duanhaochen@126.com>
2020-07-01 17:12:23 +0800

    chore: 配置@babel/plugin-proposal-logical-assignment-operators

--- packages/milktea/src/config/babelConfig.ts
+++ packages/milktea/src/config/babelConfig.ts
@@ -11,6 +11,7 @@ export function createBabelPlugins(): Array<any> {
     '@babel/plugin-proposal-class-properties',                             // class 相关
     '@babel/plugin-proposal-do-expressions',                               // do {} 语法
     '@babel/plugin-proposal-export-default-from',                          // export module from 语法
+    '@babel/plugin-proposal-logical-assignment-operators',                 // x ??= y 语法
     '@babel/plugin-proposal-nullish-coalescing-operator',                  // x ?? y 语法
     '@babel/plugin-proposal-numeric-separator',                            // 1_000_000 语法
     '@babel/plugin-proposal-optional-chaining',                            // x?.y 语法
