2b4e87f4c73c32f987e55af6c32ae4599547e572
段昊辰 <duanhaochen@126.com>
2020-07-10 10:59:55 +0800

    refactor: 修改注释

--- packages/server/src/utils/createOptions.ts
+++ packages/server/src/utils/createOptions.ts
@@ -2,7 +2,7 @@ import * as Koa from 'koa';
 import type { Context } from 'koa';
 import type { SweetOptions } from './types';
 
-/* 注入ctx.sweetOptions */
+/* 注入ctx.state.sweetOptions */
 function createSweetOptionsMiddleware(app: Koa, sweetOptions: SweetOptions): void {
   app.use(async function(ctx: Context, next: Function): Promise<void> {
     ctx.state.sweetOptions = sweetOptions;
