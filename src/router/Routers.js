daa9ee6e78f75d7feaebf42b693680b6d88dcc6e
段昊辰 <duanhaochen@126.com>
2020-08-18 17:29:00 +0800

    refactor: 放开注释的代码

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -2,13 +2,13 @@ import { useRoutes } from 'react-router-dom';
 import asyncModule from './asyncModule/index';
 import Index from '../pages/Index/index';
 
-// const SecondBundle = asyncModule(() => import(/* webpackChunkName: 'second' */ '../pages/Second/index'));
+const SecondBundle = asyncModule(() => import(/* webpackChunkName: 'second' */ '../pages/Second/index'));
 const ListBundle = asyncModule(() => import(/* webpackChunkName: 'list' */ '../pages/List/index'));
 
 function Routers(props) {
   const routes = useRoutes([
     { path: '//*', element: <Index /> },
-    // { path: 'Second/*', element: <SecondBundle /> },
+    { path: 'Second/*', element: <SecondBundle /> },
     { path: 'List/*', element: <ListBundle /> }
   ]);
 
