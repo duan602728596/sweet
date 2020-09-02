b46be8bf676d437e74c7ea7ad7a1f24206b43a5b
段昊辰 <duanhaochen@126.com>
2020-09-02 14:21:52 +0800

    test: 添加测试用例

---
+++ packages/util-tools/test/image2avif/image2avif.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { expect } from 'chai';
+import image2avif from '../../image2avif';
+
+describe('image2avif', function() {
+  it('should has avif picture files', async function() {
+    await image2avif(path.join(__dirname, 'image'), path.join(__dirname, 'output'));
+
+    const isExists = fs.existsSync(path.join(__dirname, 'output/image.avif'));
+
+    expect(isExists).to.be.true;
+  });
+});
