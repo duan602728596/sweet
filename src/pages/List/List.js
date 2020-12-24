6a8fa472e0ebb7ed4b7b40b0c929e7699643184d
段昊辰 <duanhaochen@126.com>
2020-12-24 18:41:59 +0800

    refactor: 移除redux相关

--- src/pages/List/List.js
+++ src/pages/List/List.js
@@ -1,23 +1,10 @@
 import { useState } from 'react';
 import { Observer } from 'mobx-react';
-import { useSelector, useDispatch } from 'react-redux';
-import { createSelector, createStructuredSelector } from 'reselect';
 import { Button, Table, message } from 'antd';
-import { requestList } from './reducers/reducers';
 import listStore from './models/list';
 import style from './list.sass';
 
-/* state */
-const state = createStructuredSelector({
-  dataList: createSelector(
-    ({ list }) => list.dataList,
-    (data) => data
-  )
-});
-
 function List(props) {
-  const { dataList } = useSelector(state);
-  const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
 
   // 点击加载
