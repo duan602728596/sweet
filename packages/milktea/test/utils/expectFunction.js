612870ee8fb8a4d7928a4168501e6e53b4a7d887
段昊辰 <duanhaochen@126.com>
2021-01-12 14:11:42 +0800

    test: 修改测试用例

--- packages/milktea/test/utils/expectFunction.js
+++ packages/milktea/test/utils/expectFunction.js
@@ -5,6 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import VueLoaderPlugin from 'vue-loader/dist/plugin';
 import TerserWebpackPlugin from 'terser-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 /* module属性 */
 export function expectModule(config, length) {
@@ -19,12 +20,13 @@ export function expectModule(config, length) {
 export function expectDevPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(5);
+    expect(config.plugins).to.have.lengthOf(6);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
     expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
     expect(config.plugins[2] instanceof webpack.HotModuleReplacementPlugin).to.be.true;
-    expect(config.plugins[3] instanceof HtmlWebpackPlugin).to.be.true;
-    expect(config.plugins[4] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[3] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
+    expect(config.plugins[4] instanceof HtmlWebpackPlugin).to.be.true;
+    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
   };
 }
 
@@ -32,10 +34,11 @@ export function expectDevPlugins(config) {
 export function expectDevServerPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(3);
+    expect(config.plugins).to.have.lengthOf(4);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
     expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
-    expect(config.plugins[2] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[2] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
+    expect(config.plugins[3] instanceof webpack.ProgressPlugin).to.be.true;
   };
 }
 
@@ -43,13 +46,14 @@ export function expectDevServerPlugins(config) {
 export function expectProPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(6);
+    expect(config.plugins).to.have.lengthOf(7);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
     expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
     expect(config.plugins[2] instanceof MiniCssExtractPlugin).to.be.true;
-    expect(config.plugins[3] instanceof HtmlWebpackPlugin).to.be.true;
-    expect(config.plugins[4] instanceof VueLoaderPlugin).to.be.true;
-    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[3] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
+    expect(config.plugins[4] instanceof HtmlWebpackPlugin).to.be.true;
+    expect(config.plugins[5] instanceof VueLoaderPlugin).to.be.true;
+    expect(config.plugins[6] instanceof webpack.ProgressPlugin).to.be.true;
   };
 }
 
@@ -57,12 +61,13 @@ export function expectProPlugins(config) {
 export function expectProServerPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(5);
+    expect(config.plugins).to.have.lengthOf(6);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
     expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
     expect(config.plugins[2] instanceof MiniCssExtractPlugin).to.be.true;
-    expect(config.plugins[3] instanceof VueLoaderPlugin).to.be.true;
-    expect(config.plugins[4] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[3] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
+    expect(config.plugins[4] instanceof VueLoaderPlugin).to.be.true;
+    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
   };
 }
 
