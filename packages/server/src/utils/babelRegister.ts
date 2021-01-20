fea0f595718acfa05865cab39be2a7125b5f5ea1
段昊辰 <duanhaochen@126.com>
2021-01-20 16:28:22 +0800

    refactor: 使用@sweet-milktea/babel-preset-sweet插件

--- packages/server/src/utils/babelRegister.ts
+++ packages/server/src/utils/babelRegister.ts
@@ -1,8 +1,8 @@
+import { requireModule } from './utils';
 import type { SweetOptions } from './types';
 
 interface RegisterConfig {
   presets: Array<any>;
-  plugins: Array<any>;
   cache: boolean;
   ignore: Array<RegExp>;
   extensions: Array<string>;
@@ -15,44 +15,15 @@ function createRegisterConfig(): RegisterConfig {
   return {
     presets: [
       [
-        '@babel/preset-env',
+        requireModule('@sweet-milktea/babel-preset-sweet'),
         {
-          targets: { browsers: ['node 10'] },
-          debug: false,
-          modules: 'commonjs',
-          useBuiltIns: 'usage',
-          corejs: 3,
-          bugfixes: true
-        }
-      ],
-      [
-        '@babel/preset-typescript',
-        {
-          isTSX: true,
-          allExtensions: true,
-          allowNamespaces: true
-        }
-      ]
-    ],
-    plugins: [
-      ['@babel/plugin-proposal-decorators', { legacy: true }],               // 修饰器
-      '@babel/plugin-proposal-class-static-block',                           // class static块
-      '@babel/plugin-proposal-class-properties',                             // class 相关
-      '@babel/plugin-proposal-do-expressions',                               // do {} 语法
-      '@babel/plugin-proposal-export-default-from',                          // export module from 语法
-      '@babel/plugin-proposal-export-namespace-from',                        // export * as module from 语法
-      '@babel/plugin-proposal-numeric-separator',                            // 数字分隔符
-      ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }], // 管道函数
-      '@babel/plugin-proposal-throw-expressions',                            // var e = throw new Error(err) 语法
-      '@babel/plugin-syntax-module-string-names',                            // import { 'unicode' as bar } and export { foo as 'unicode' }
-      '@babel/plugin-syntax-top-level-await',                                // top-level await
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          corejs: { version: 3, proposals: true },
-          helpers: true,
-          regenerator: false,
-          useESModules: true
+          env: {
+            nodeEnv: true,
+            modules: 'commonjs'
+          },
+          typescript: {
+            use: true
+          }
         }
       ]
     ],
