f712d745572ff49c80880fc5f35e9d86904d8277
段昊辰 <duanhaochen@126.com>
2021-01-20 18:03:08 +0800

    test: 添加测试用例

---
+++ packages/babel-preset-sweet/test/babelPresetSweet.js
@@ -0,0 +1,42 @@
+import { promisify } from 'util';
+import { transform } from '@babel/core';
+import { expect } from 'chai';
+
+const transformPromise = promisify(transform);
+
+/* 编译代码 */
+function transformCode(code, options) {
+  return transformPromise(code, {
+    presets: [
+      [require('../cjs'), options]
+    ]
+  });
+}
+
+describe('babel-preset-sweet', function() {
+  it('build javascript', async function() {
+    const { code } = await transformCode(`const a = 5;
+    const b = <div></div>;`);
+
+    expect(code.includes('var a = 5;')).to.be.true;
+    expect(code.includes('jsx')).to.be.true;
+  });
+
+  it('build ecmascript', async function() {
+    const { code } = await transformCode(`const a = 5;
+    async function func() {}`, {
+      env: { ecmascript: true }
+    });
+
+    expect(code.includes('const a = 5;')).to.be.true;
+    expect(code.includes('async function')).to.be.true;
+  });
+
+  it('build typescript', async function() {
+    const { code } = await transformCode('const a: number = 5;', {
+      typescript: { use: true }
+    });
+
+    expect(code.includes('var a = 5;')).to.be.true;
+  });
+});
