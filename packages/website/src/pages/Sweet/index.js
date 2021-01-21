1cf9058ea5d9af66499a28e40d771e790361b21f
段昊辰 <duanhaochen@126.com>
2021-01-21 16:51:38 +0800

    refactor: 添加v1地址跳转

--- packages/website/src/pages/Sweet/index.js
+++ packages/website/src/pages/Sweet/index.js
@@ -1,8 +1,11 @@
 import { useRoutes } from 'react-router-dom';
+import { Select } from 'antd';
+import style from './index.sass';
 import Element from '../../components/Element/Element';
 import MarkDown from '../../components/MarkDown/MarkDown';
 import introduction from '../../../../../README.md';
 import quickStart from './quick-start.md';
+import lernaJson from '../../../../../lerna.json';
 
 const introductionText = introduction
   .replace('statics/logo.svg', 'https://raw.githubusercontent.com/duan602728596/sweet/master/statics/logo.svg')
@@ -10,11 +13,28 @@ const introductionText = introduction
 
 /* /Sweet 路由 */
 function Index(props) {
+  // 打开v1地址
+  function handleOpenOldWebSite(value) {
+    if (value === 'v1') {
+      window.open('https://duan602728596.github.io/sweet/v1/#/Sweet/Introduction');
+    }
+  }
+
   const routes = useRoutes([
     {
       path: 'Introduction',
       element: (
         <Element title="介绍">
+          <div className={ style.version }>
+            <Select className={ style.versionSelect }
+              size="small"
+              defaultValue={ lernaJson.version }
+              onSelect={ handleOpenOldWebSite }
+            >
+              <Select.Option value={ lernaJson.version }>{ lernaJson.version }</Select.Option>
+              <Select.Option value="v1">v1</Select.Option>
+            </Select>
+          </div>
           <MarkDown markdown={ introductionText } />
         </Element>
       )
