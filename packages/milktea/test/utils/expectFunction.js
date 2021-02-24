2525482628442536af124435f6cb0ac71866ded6
段昊辰 <duanhaochen@126.com>
2021-02-24 09:47:28 +0800

    test: 修改测试用例

--- packages/milktea/test/utils/expectFunction.js
+++ packages/milktea/test/utils/expectFunction.js
@@ -5,6 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import VueLoaderPlugin from 'vue-loader/dist/plugin';
 import TerserWebpackPlugin from 'terser-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 /* module属性 */
@@ -20,13 +21,14 @@ export function expectModule(config, length) {
 export function expectDevPlugins(config) {
   return function() {
     expect(config.plugins).to.be.an('array');
-    expect(config.plugins).to.have.lengthOf(6);
+    expect(config.plugins).to.have.lengthOf(7);
     expect(config.plugins[0] instanceof webpack.IgnorePlugin).to.be.true;
     expect(config.plugins[1] instanceof webpack.DefinePlugin).to.be.true;
     expect(config.plugins[2] instanceof webpack.HotModuleReplacementPlugin).to.be.true;
-    expect(config.plugins[3] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
-    expect(config.plugins[4] instanceof HtmlWebpackPlugin).to.be.true;
-    expect(config.plugins[5] instanceof webpack.ProgressPlugin).to.be.true;
+    expect(config.plugins[3] instanceof ReactRefreshWebpackPlugin).to.be.true;
+    expect(config.plugins[4] instanceof ForkTsCheckerWebpackPlugin).to.be.true;
+    expect(config.plugins[5] instanceof HtmlWebpackPlugin).to.be.true;
+    expect(config.plugins[6] instanceof webpack.ProgressPlugin).to.be.true;
   };
 }
 
