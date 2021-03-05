8da26ad6c09b3758ad096511471d5256513d8144
段昊辰 <duanhaochen@126.com>
2021-03-05 14:46:18 +0800

    refactor: 添加function-bind语法插件

--- packages/babel-preset-sweet/src/utils.ts
+++ packages/babel-preset-sweet/src/utils.ts
@@ -6,6 +6,7 @@ export const defaultPlugins: Array<any> = [
   '@babel/plugin-proposal-do-expressions',                                 // do {} 语法
   '@babel/plugin-proposal-export-default-from',                            // export module from 语法
   '@babel/plugin-proposal-export-namespace-from',                          // export * as module from 语法
+  '@babel/plugin-proposal-function-bind',                                  // obj::func(val) 语法
   '@babel/plugin-proposal-numeric-separator',                              // 数字分隔符
   ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],   // 管道函数
   '@babel/plugin-proposal-throw-expressions',                              // var e = throw new Error(err) 语法
