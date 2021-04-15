ab97312a8d111d3c9f5e81377106c7a4af51bdc9
段昊辰 <duanhaochen@126.com>
2021-04-15 10:43:05 +0800

    refactor: 优化代码

--- packages/server/src/utils/createHttpsCertificate.ts
+++ packages/server/src/utils/createHttpsCertificate.ts
@@ -7,16 +7,14 @@ export type HttpsCertificate = [boolean, Buffer | undefined, Buffer | undefined]
 
 /* 创建https证书 */
 async function createHttpsCertificate(sweetOptions: SweetOptions, httpsKey: string, httpsCert: string): Promise<HttpsCertificate> {
-  const key: string = path.isAbsolute(httpsKey)
-    ? httpsKey
-    : path.join(sweetOptions.basicPath, httpsKey);
-  const cert: string = path.isAbsolute(httpsCert)
-    ? httpsCert
-    : path.join(sweetOptions.basicPath, httpsCert);
-
+  const [key, cert]: [string, string] = [
+    path.isAbsolute(httpsKey) ? httpsKey : path.join(sweetOptions.basicPath, httpsKey),
+    path.isAbsolute(httpsCert) ? httpsCert : path.join(sweetOptions.basicPath, httpsCert)
+  ];
   const useHttps: boolean = (await isExists(key)) && (await isExists(cert));
-  const keyFile: Buffer | undefined = useHttps ? await fs.promises.readFile(key) : undefined;
-  const certFile: Buffer | undefined = useHttps ? await fs.promises.readFile(cert) : undefined;
+  const [keyFile, certFile]: [Buffer | undefined, Buffer | undefined] = useHttps
+    ? await Promise.all([fs.promises.readFile(key), fs.promises.readFile(cert)])
+    : [undefined, undefined];
 
   return [useHttps, keyFile, certFile];
 }
