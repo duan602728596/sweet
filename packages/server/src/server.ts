6efc2b211d9453f3e77e817e6e12ff871f07ed42
段昊辰 <duanhaochen@126.com>
2021-04-28 17:11:36 +0800

    chore: 导出模块

---
+++ packages/server/src/server.ts
@@ -0,0 +1,9 @@
+import devServer from './devServer';
+import proServer from './proServer';
+import hotClientEntry from './hotClientEntry';
+
+export default {
+  devServer,
+  proServer,
+  hotClientEntry
+};
