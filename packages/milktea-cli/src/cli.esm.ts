18faae9530ca194837fadeb5718d1b02bfeee3fc
段昊辰 <duanhaochen@126.com>
2021-04-29 13:50:05 +0800

    feat: 调整cli命令

---
+++ packages/milktea-cli/src/cli.esm.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/* 命令行工具 */
+import * as process from 'process';
+// @ts-ignore
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import command from './command';
+
+command(yargs(hideBin(process.argv)));
