3dff008336abd70e68db79c1aa6a54c76525c07b
段昊辰 <duanhaochen@126.com>
2021-04-29 18:04:29 +0800

    fix: 调整模块的加载

--- packages/server/src/utils/moduleUtils.esm.ts
+++ packages/server/src/utils/moduleUtils.esm.ts
@@ -6,8 +6,9 @@ const require: NodeRequire = createRequire(import.meta.url);
 /* 模块导入 */
 export async function requireModule(id: string): Promise<any> {
   const module: { default: any } | any = await import(id);
+  const dModule: any = 'default' in module ? module['default'] : module;
 
-  return 'default' in module ? module.default : module;
+  return 'default' in dModule ? dModule.default : dModule;
 }
 
 /* 判断模块是否存在 */
@@ -21,7 +22,7 @@ export function moduleExists(id: string): string | false {
 
 /* 模块导入并且清除缓存 */
 export async function deleteCacheAndRequireModule(id: string): Promise<any> {
-  const idAndTime: string = `${ id }${ new Date().getTime() }`;
+  const idAndTime: string = `${ id }?${ new Date().getTime() }`;
 
   return await requireModule(idAndTime);
 }
