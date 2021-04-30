d7a00366c5bb1906abef40fe6ebf9623e1357a42
段昊辰 <duanhaochen@126.com>
2021-04-30 18:03:42 +0800

    test: 测试用例

--- packages/utils/test/test.esm.mjs
+++ packages/utils/test/test.esm.mjs
@@ -1,8 +1,13 @@
 import path from 'path';
 import { expect } from 'chai';
-import { requireModule, requireCommonjsModule } from '../esm/index.js';
+import {
+  requireModule,
+  requireCommonjsModule,
+  requireJson
+} from '../esm/index.js';
 
-const __dirname = decodeURIComponent(path.dirname(import.meta.url.replace(/^file:\/{2}/, '')));
+const __dirname = path.dirname(
+  decodeURIComponent(import.meta.url.replace(/^file:\/{2}/, '')));
 
 describe('esm test', function() {
   it('requireModule test', async function() {
@@ -16,7 +21,19 @@ describe('esm test', function() {
 
   it('requireCommonjsModule test', async function() {
     const m1 = await requireCommonjsModule(path.join(__dirname, 'cjsUtils/module.js'));
+    const m2 = await requireCommonjsModule(path.join(__dirname, 'cjsUtils/module.js'), true);
 
     expect(m1).to.eql('cjs module');
+    expect(m2.default).to.eql('cjs module');
+    expect(m2.name).to.eql('cjs module name');
+  });
+
+  it('requireJson test', async function() {
+    const m = await requireJson(path.join(__dirname, 'esmUtils/json.json'));
+
+    expect(m).to.eql({
+      "filename": "json",
+      "data": 12
+    });
   });
 });
