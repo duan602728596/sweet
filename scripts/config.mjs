c46e888cd583cb8a00891956ae61b0fe17390b84
段昊辰 <duanhaochen@126.com>
2021-05-07 12:14:38 +0800

    build: 调整dirname的解析

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -1,8 +1,9 @@
 /* 通用配置 */
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-export const __dirname = path.dirname(
-  decodeURIComponent(import.meta.url.replace(/^file:\/{2}/, '')));
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
 
 export const dir = path.join(__dirname, '../packages');
 
