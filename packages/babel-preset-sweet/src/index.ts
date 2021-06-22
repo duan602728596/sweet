3fc38ad45a3cd5f081a177f29a284ec0c4aa00f2
段昊辰 <duanhaochen@126.com>
2021-06-22 11:24:41 +0800

    refactor: 移除插件

--- packages/babel-preset-sweet/src/index.ts
+++ packages/babel-preset-sweet/src/index.ts
@@ -20,9 +20,7 @@ export const defaultPlugins: Array<any> = [
   '@babel/plugin-proposal-export-default-from',   // export module from 语法
   '@babel/plugin-proposal-export-namespace-from', // export * as module from 语法
   '@babel/plugin-proposal-function-bind',         // obj::func(val) 语法
-  '@babel/plugin-proposal-function-sent',         // function* generator() { console.log(function.sent, yield) } 语法
   '@babel/plugin-proposal-numeric-separator',     // 数字分隔符
-  '@babel/plugin-proposal-partial-application',   // function add(x, y) { return x + y; } add(1, ?) 语法
   ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }], // 管道函数
   '@babel/plugin-proposal-throw-expressions',     // var e = throw new Error(err) 语法
   '@babel/plugin-syntax-module-string-names',     // import { 'unicode' as bar } and export { foo as 'unicode' }
