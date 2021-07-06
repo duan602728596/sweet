c4560000186d127ea6ccb037bd9006c0e13e51fb
段昊辰 <duanhaochen@126.com>
2021-07-06 10:22:03 +0800

    test: 补充测试用例

--- packages/util-tools/test/afterTest.mjs
+++ packages/util-tools/test/afterTest.mjs
@@ -7,7 +7,10 @@ const rimrafPromise = util.promisify(rimraf);
 const { __dirname } = metaHelper(import.meta.url);
 
 async function afterTest() {
-  await rimrafPromise(path.join(__dirname, '/**/output'));
+  await Promise.all([
+    rimrafPromise(path.join(__dirname, '/**/output')),
+    rimrafPromise(path.join(__dirname, '/**/output-sharp'))
+  ]);
 }
 
 export default afterTest;
--- packages/util-tools/test/image2avif/image2avif.mjs
+++ packages/util-tools/test/image2avif/image2avif.mjs
@@ -7,10 +7,18 @@ import image2avif from '../../image2avif.js';
 const { __dirname } = metaHelper(import.meta.url);
 
 describe('image2avif', function() {
-  it('should has avif picture files', async function() {
-    await image2avif(path.join(__dirname, 'image'), path.join(__dirname, 'output'), true);
+  it('should has avif picture files, conversion used avifenc', async function() {
+    await image2avif(path.join(__dirname, 'image'), path.join(__dirname, 'output'), 'avifenc', true);
 
     const isExists = fs.existsSync(path.join(__dirname, 'output/image.avif'));
+
+    expect(isExists).to.be.true;
+  });
+
+  it('should has avif picture files, conversion used sharp', async function() {
+    await image2avif(path.join(__dirname, 'image-sharp'), path.join(__dirname, 'output-sharp'), 'sharp', true);
+
+    const isExists = fs.existsSync(path.join(__dirname, 'output-sharp/image.avif'));
 
     expect(isExists).to.be.true;
   });
