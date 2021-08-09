d61566c466bc3273b49048fbaa4d345e255c61ef
段昊辰 <duanhaochen@126.com>
2021-08-09 10:36:17 +0800

    refactor: 移除默认已加载的插件

--- packages/babel-preset-sweet/src/utils/defaultPlugins.ts
+++ packages/babel-preset-sweet/src/utils/defaultPlugins.ts
@@ -6,13 +6,10 @@ const defaultPlugins: Array<any> = [
   '@babel/plugin-proposal-class-properties',      // class 相关
   '@babel/plugin-proposal-do-expressions',        // do {} 语法
   '@babel/plugin-proposal-export-default-from',   // export module from 语法
-  '@babel/plugin-proposal-export-namespace-from', // export * as module from 语法
   '@babel/plugin-proposal-function-bind',         // obj::func(val) 语法
-  '@babel/plugin-proposal-numeric-separator',     // 数字分隔符
   ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }], // 管道函数
   '@babel/plugin-proposal-throw-expressions',     // var e = throw new Error(err) 语法
-  '@babel/plugin-syntax-module-string-names',     // import { 'unicode' as bar } and export { foo as 'unicode' }
-  '@babel/plugin-syntax-top-level-await'          // top-level await
+  '@babel/plugin-syntax-module-string-names'      // import { 'unicode' as bar } and export { foo as 'unicode' }
 ];
 
 export default defaultPlugins;
