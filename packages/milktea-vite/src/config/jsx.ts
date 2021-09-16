2082646aad2328117958b65721efb8714c8d96b8
段昊辰 <duanhaochen@126.com>
2021-09-16 13:52:35 +0800

    refactor: 修改名称

--- packages/milktea-vite/src/config/jsx.ts
+++ packages/milktea-vite/src/config/jsx.ts
@@ -2,10 +2,15 @@ import { requireModule } from '@sweet-milktea/utils';
 import type { Plugin } from 'vite';
 
 /* esbuild的react配置 */
+enum jsx {
+  factory = '__react$jsx_0__',
+  fragment = '__react$Fragment_0__'
+}
+
 export const esbuildReact: object = {
-  jsxFactory: '__react$jsx__',
-  jsxFragment: '__react$Fragment__',
-  jsxInject: "import { createElement as __react$jsx__, Fragment as __react$Fragment__ } from 'react';"
+  jsxFactory: jsx.factory,
+  jsxFragment: jsx.fragment,
+  jsxInject: `import { createElement as ${ jsx.factory }, Fragment as ${ jsx.fragment } } from 'react';`
 };
 
 /* vite的vue配置 */
