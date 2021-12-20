66ae5276dd54bb0f4d33ce48d1aa20de7c37030e
段昊辰 <duanhaochen@126.com>
2021-12-20 15:36:45 +0800

    refactor: 添加一个注释

--- packages/utils/importESM.js
+++ packages/utils/importESM.js
@@ -3,6 +3,7 @@ const { pathToFileURL } = require('url');
 
 /**
  * 异步加载ESM模块
+ * 关于使用ESM模块的讨论：https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
  * @param { string } id: 模块id
  * @return { Promise<any> }
  */
