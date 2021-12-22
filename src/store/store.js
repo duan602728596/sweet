8efdd9fc5b4ea1bbce37ac30e3e6fec9ebaea989
段昊辰 <duanhaochen@126.com>
2021-12-22 17:10:17 +0800

    perf: 添加默认参数

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import { modules } from './modules';
 
 export let store;
 
-function createStore(initialState) {
+function createStore(initialState = {}) {
   store = new Vuex.Store({
     state: initialState,
     getters: {
