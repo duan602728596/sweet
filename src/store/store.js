855c4bb9ae5ed692b52ed401160a03d333786a81
段昊辰 <duanhaochen@126.com>
2021-12-22 17:05:46 +0800

    perf: 提取到专门的函数中

--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,20 @@ import { modules } from './modules';
 
 export let store;
 
+function createStore(initialState) {
+  store = new Vuex.Store({
+    state: initialState,
+    getters: {
+      getInitialStateData: (state) => (key) => state[key]
+    },
+    modules
+  });
+}
+
 /* 创建store */
 export function storeFactory(initialState = {}) {
   if (!store) {
-    store = new Vuex.Store({
-      state: initialState,
-      getters: {
-        getInitialStateData: (state) => (key) => state[key]
-      },
-      modules
-    });
+    createStore(initialState);
   }
 
   return store;
