9d103cab25c4b8ee1f8c634c6ba4b44d1337e3cf
段昊辰 <duanhaochen@126.com>
2022-02-09 16:08:27 +0800

    refactor: 删除文件

--- packages/milktea/src/plugins/requirePlugins.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import { requireModule } from '@sweet-milktea/utils';
-
-/* 加载插件 */
-function requirePlugins<T = unknown>(id: string): Promise<T> {
-  return requireModule(path.join(__dirname, id));
-}
-
-export default requirePlugins;
