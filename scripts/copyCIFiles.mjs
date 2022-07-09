f0db2d031fbfc279d0ab64ff79b7deb1540eab77
段昊辰 <duanhaochen@126.com>
2022-07-09 21:18:52 +0800

    ci: 添加拷贝的文件

--- scripts/copyCIFiles.mjs
+++ scripts/copyCIFiles.mjs
@@ -12,6 +12,7 @@ await Promise.all(
   )).concat([
     fse.copy(path.join(dir, 'website'), path.join(websiteCI, 'packages/website')),
     fse.copy(path.join(__dirname, '../tsconfig.json'), path.join(websiteCI, 'tsconfig.json')),
-    fse.copy(path.join(__dirname, '../lerna.json'), path.join(websiteCI, 'lerna.json'))
+    fse.copy(path.join(__dirname, '../lerna.json'), path.join(websiteCI, 'lerna.json')),
+    fse.copy(path.join(__dirname, '../README.md'), path.join(websiteCI, 'README.md'))
   ])
 );
