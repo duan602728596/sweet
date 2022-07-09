c241c72957cb3c28eee466d08aae8b6b09e0b187
段昊辰 <duanhaochen@126.com>
2022-07-09 20:58:56 +0800

    ci: 修改ci脚本

---
+++ scripts/copyCIFiles.mjs
@@ -0,0 +1,17 @@
+import path from 'node:path';
+import fse from 'fs-extra';
+import { dir, packageNames, __dirname } from './config.mjs';
+
+const websiteCI = path.join(__dirname, '../.website-ci');
+
+// 拷贝目录
+await Promise.all(
+  packageNames.map((name) => fse.copy(
+    path.join(dir, name, 'README.md'),
+    path.join(websiteCI, 'packages', name, 'README.md')
+  )).concat([
+    fse.copy(path.join(dir, 'website'), path.join(websiteCI, 'packages/website')),
+    fse.copy(path.join(__dirname, '../tsconfig.json'), path.join(websiteCI, 'tsconfig.json')),
+    fse.copy(path.join(__dirname, '../lerna.json'), path.join(websiteCI, 'lerna.json'))
+  ])
+);
