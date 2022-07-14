8574878d91e1d872075a1c0b260ed1d8f4bd713b
段昊辰 <duanhaochen@126.com>
2022-07-14 23:14:01 +0800

    refactor: 修改模块的加载

--- packages/milktea/src/plugins/handleProgress.ts
+++ packages/milktea/src/plugins/handleProgress.ts
@@ -1,14 +1,4 @@
-// @ts-ignore Node16
-import type Chalk from 'chalk';
-
-/**
- * chalk
- */
-async function getChalk(): Promise<typeof Chalk> {
-  const chalkModule: { default: typeof Chalk } = await import('chalk');
-
-  return chalkModule.default;
-}
+import chalk from 'chalk';
 
 /**
  * 计算进度百分比
@@ -22,22 +12,16 @@ function calculateProgress(percentage: number): string {
 }
 
 /* 格式化dll的输出 */
-export async function handleDllProgress(percentage: number, message: string, ...args: Array<string>): Promise<void> {
-  const chalk: typeof Chalk = await getChalk();
-
+export function handleDllProgress(percentage: number, message: string, ...args: Array<string>): void {
   console.info(chalk.white.bgGreen(`Dll: ${ calculateProgress(percentage) }%`), message, ...args);
 }
 
 /* 格式化输出 */
-export async function handleDefaultProgress(percentage: number, message: string, ...args: Array<string>): Promise<void> {
-  const chalk: typeof Chalk = await getChalk();
-
+export function handleDefaultProgress(percentage: number, message: string, ...args: Array<string>): void {
   console.info(chalk.white.bgGreen(`Client: ${ calculateProgress(percentage) }%`), message, ...args);
 }
 
 /* 服务端渲染的格式化输出 */
-export async function handleServerRenderProgress(percentage: number, message: string, ...args: Array<string>): Promise<void> {
-  const chalk: typeof Chalk = await getChalk();
-
+export function handleServerRenderProgress(percentage: number, message: string, ...args: Array<string>): void {
   console.info(chalk.white.bgBlue(`Server: ${ calculateProgress(percentage) }%`), message, ...args);
 }
