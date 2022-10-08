7d4fbde630b0bc2b07d66ad6babe24885c3b6841
段昊辰 <duanhaochen@126.com>
2022-10-08 11:46:32 +0800

    refactor: 修改修饰器的语法配置

--- packages/babel-preset-sweet/src/utils/defaultPlugins.ts
+++ packages/babel-preset-sweet/src/utils/defaultPlugins.ts
@@ -3,7 +3,7 @@ import type { PluginItem } from '@babel/core';
 /* 默认加载的插件 */
 const defaultPlugins: Array<PluginItem> = [
   '@babel/plugin-proposal-async-do-expressions',  // async do {} 语法
-  ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }], // 修饰器
+  '@babel/plugin-proposal-decorators',            // 修饰器
   '@babel/plugin-proposal-destructuring-private', // var { #y: y } = this 语法
   '@babel/plugin-proposal-class-properties',      // class 相关
   '@babel/plugin-proposal-do-expressions',        // do {} 语法
