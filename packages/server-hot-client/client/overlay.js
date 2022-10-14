92b3de5c0a34292d63b72cd86be9fb588a3ee2d2
段昊辰 <duanhaochen@126.com>
2022-10-14 23:38:53 +0800

    build: 编译文件

--- packages/server-hot-client/client/overlay.js
+++ packages/server-hot-client/client/overlay.js
@@ -1,7 +1,14 @@
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // The error overlay is inspired (and mostly copied) from Create React App (https://github.com/facebookincubator/create-react-app)
 // They, in turn, got inspired by webpack-hot-middleware (https://github.com/glenjamin/webpack-hot-middleware).
 import ansiHTML from "ansi-html-community";
 import { encode } from "html-entities";
+import { containerStyle, dismissButtonStyle, headerStyle, iframeStyle, msgStyles, msgTextStyle, msgTypeStyle } from "./overlay/styles.js";
 var colors = {
   reset: ["transparent", "transparent"],
   black: "181818",
@@ -28,8 +35,20 @@ var onLoadQueue = [];
 var overlayTrustedTypesPolicy;
 ansiHTML.setColors(colors);
 /**
+ *
+ * @param {HTMLElement} element
+ * @param {CSSStyleDeclaration} style
+ */
+
+function applyStyle(element, style) {
+  Object.keys(style).forEach(function (prop) {
+    element.style[prop] = style[prop];
+  });
+}
+/**
  * @param {string | null} trustedTypesPolicyName
  */
+
 
 function createContainer(trustedTypesPolicyName) {
   // Enable Trusted Types if they are available in the current browser.
@@ -44,15 +63,7 @@ function createContainer(trustedTypesPolicyName) {
   iframeContainerElement = document.createElement("iframe");
   iframeContainerElement.id = "webpack-dev-server-client-overlay";
   iframeContainerElement.src = "about:blank";
-  iframeContainerElement.style.position = "fixed";
-  iframeContainerElement.style.left = 0;
-  iframeContainerElement.style.top = 0;
-  iframeContainerElement.style.right = 0;
-  iframeContainerElement.style.bottom = 0;
-  iframeContainerElement.style.width = "100vw";
-  iframeContainerElement.style.height = "100vh";
-  iframeContainerElement.style.border = "none";
-  iframeContainerElement.style.zIndex = 9999999999;
+  applyStyle(iframeContainerElement, iframeStyle);
 
   iframeContainerElement.onload = function () {
     containerElement =
@@ -61,42 +72,19 @@ function createContainer(trustedTypesPolicyName) {
     /** @type {HTMLIFrameElement} */
     iframeContainerElement.contentDocument.createElement("div");
     containerElement.id = "webpack-dev-server-client-overlay-div";
-    containerElement.style.position = "fixed";
-    containerElement.style.boxSizing = "border-box";
-    containerElement.style.left = 0;
-    containerElement.style.top = 0;
-    containerElement.style.right = 0;
-    containerElement.style.bottom = 0;
-    containerElement.style.width = "100vw";
-    containerElement.style.height = "100vh";
-    containerElement.style.backgroundColor = "rgba(0, 0, 0, 0.85)";
-    containerElement.style.color = "#E8E8E8";
-    containerElement.style.fontFamily = "Menlo, Consolas, monospace";
-    containerElement.style.fontSize = "large";
-    containerElement.style.padding = "2rem";
-    containerElement.style.lineHeight = "1.2";
-    containerElement.style.whiteSpace = "pre-wrap";
-    containerElement.style.overflow = "auto";
-    var headerElement = document.createElement("span");
+    applyStyle(containerElement, containerStyle);
+    var headerElement = document.createElement("div");
     headerElement.innerText = "Compiled with problems:";
+    applyStyle(headerElement, headerStyle);
     var closeButtonElement = document.createElement("button");
-    closeButtonElement.innerText = "X";
-    closeButtonElement.style.background = "transparent";
-    closeButtonElement.style.border = "none";
-    closeButtonElement.style.fontSize = "20px";
-    closeButtonElement.style.fontWeight = "bold";
-    closeButtonElement.style.color = "white";
-    closeButtonElement.style.cursor = "pointer";
-    closeButtonElement.style.cssFloat = "right"; // @ts-ignore
-
-    closeButtonElement.style.styleFloat = "right";
+    applyStyle(closeButtonElement, dismissButtonStyle);
+    closeButtonElement.innerText = "×";
+    closeButtonElement.ariaLabel = "Dismiss";
     closeButtonElement.addEventListener("click", function () {
       hide();
     });
     containerElement.appendChild(headerElement);
     containerElement.appendChild(closeButtonElement);
-    containerElement.appendChild(document.createElement("br"));
-    containerElement.appendChild(document.createElement("br"));
     /** @type {Document} */
 
     /** @type {HTMLIFrameElement} */
@@ -177,7 +165,7 @@ function formatProblem(type, item) {
 
 /**
  * @param {string} type
- * @param {Array<string  | { file?: string, moduleName?: string, loc?: string, message?: string }>} messages
+ * @param {Array<string  | { moduleIdentifier?: string, moduleName?: string, loc?: string, message?: string }>} messages
  * @param {string | null} trustedTypesPolicyName
  */
 
@@ -186,24 +174,36 @@ function show(type, messages, trustedTypesPolicyName) {
   ensureOverlayExists(function () {
     messages.forEach(function (message) {
       var entryElement = document.createElement("div");
-      var typeElement = document.createElement("span");
+      var msgStyle = type === "warning" ? msgStyles.warning : msgStyles.error;
+      applyStyle(entryElement, _objectSpread(_objectSpread({}, msgStyle), {}, {
+        padding: "1rem 1rem 1.5rem 1rem"
+      }));
+      var typeElement = document.createElement("div");
 
       var _formatProblem = formatProblem(type, message),
           header = _formatProblem.header,
           body = _formatProblem.body;
 
       typeElement.innerText = header;
-      typeElement.style.color = "#".concat(colors.red); // Make it look similar to our terminal.
+      applyStyle(typeElement, msgTypeStyle);
+
+      if (message.moduleIdentifier) {
+        applyStyle(typeElement, {
+          cursor: "pointer"
+        });
+        typeElement.dataset.canOpen = true;
+        typeElement.addEventListener("click", function () {
+          fetch("/webpack-dev-server/open-editor?fileName=".concat(message.moduleIdentifier));
+        });
+      } // Make it look similar to our terminal.
+
 
       var text = ansiHTML(encode(body));
       var messageTextNode = document.createElement("div");
+      applyStyle(messageTextNode, msgTextStyle);
       messageTextNode.innerHTML = overlayTrustedTypesPolicy ? overlayTrustedTypesPolicy.createHTML(text) : text;
       entryElement.appendChild(typeElement);
-      entryElement.appendChild(document.createElement("br"));
-      entryElement.appendChild(document.createElement("br"));
       entryElement.appendChild(messageTextNode);
-      entryElement.appendChild(document.createElement("br"));
-      entryElement.appendChild(document.createElement("br"));
       /** @type {HTMLDivElement} */
 
       containerElement.appendChild(entryElement);
