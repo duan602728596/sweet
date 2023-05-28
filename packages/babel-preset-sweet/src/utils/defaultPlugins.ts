3649275091b4c1d40bdf33bf545ef7c7c8df1e49
段昊辰 <duanhaochen@126.com>
2023-05-28 11:58:34 +0800

    feat: 修改默认加载的插件

--- packages/babel-preset-sweet/src/utils/defaultPlugins.ts
+++ packages/babel-preset-sweet/src/utils/defaultPlugins.ts
@@ -5,14 +5,12 @@ const defaultPlugins: Array<PluginItem> = [
   '@babel/plugin-proposal-async-do-expressions',  // async do {} 语法
   ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: false }], // 修饰器
   '@babel/plugin-proposal-destructuring-private', // var { #y: y } = this 语法
-  '@babel/plugin-proposal-class-properties',      // class 相关
   '@babel/plugin-proposal-do-expressions',        // do {} 语法
   '@babel/plugin-proposal-export-default-from',   // export module from 语法
   '@babel/plugin-proposal-function-bind',         // obj::func(val) 语法
   ['@babel/plugin-proposal-pipeline-operator', { proposal: 'hack', topicToken: '#' }], // 管道函数
   '@babel/plugin-proposal-throw-expressions',     // var e = throw new Error(err) 语法
-  '@babel/plugin-syntax-import-assertions',       // import json from './foo.json' assert { type: 'json' }
-  '@babel/plugin-syntax-module-string-names'      // import { 'unicode' as bar } and export { foo as 'unicode' }
+  '@babel/plugin-syntax-import-assertions'        // import json from './foo.json' assert { type: 'json' }
 ];
 
 export default defaultPlugins;
