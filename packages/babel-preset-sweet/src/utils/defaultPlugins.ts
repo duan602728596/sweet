f85bb7e097a20b25410dce562f131ad2084e7be5
段昊辰 <duanhaochen@126.com>
2023-08-17 13:35:51 +0800

    style: 修改代码

--- packages/babel-preset-sweet/src/utils/defaultPlugins.ts
+++ packages/babel-preset-sweet/src/utils/defaultPlugins.ts
@@ -2,16 +2,26 @@ import type { PluginItem } from '@babel/core';
 
 /* 默认加载的插件 */
 const defaultPlugins: Array<PluginItem> = [
-  '@babel/plugin-proposal-async-do-expressions',  // async do {} 语法
-  ['@babel/plugin-proposal-decorators', { version: '2023-05' }], // 修饰器
-  '@babel/plugin-proposal-destructuring-private', // var { #y: y } = this 语法
-  '@babel/plugin-proposal-do-expressions',        // do {} 语法
-  '@babel/plugin-proposal-explicit-resource-management', // using handlerSync = openSync()
-  '@babel/plugin-proposal-export-default-from',   // export module from 语法
-  '@babel/plugin-proposal-function-bind',         // obj::func(val) 语法
-  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'hack', topicToken: '#' }], // 管道函数
-  '@babel/plugin-proposal-throw-expressions',     // var e = throw new Error(err) 语法
-  ['@babel/plugin-syntax-import-assertions', { deprecatedAssertSyntax: true }] // import json from './foo.json' with { type: 'json' }
+  // async do {} 语法
+  '@babel/plugin-proposal-async-do-expressions',
+  // 修饰器
+  ['@babel/plugin-proposal-decorators', { version: '2023-05' }],
+  // var { #y: y } = this 语法
+  '@babel/plugin-proposal-destructuring-private',
+  // do {} 语法
+  '@babel/plugin-proposal-do-expressions',
+  // using handlerSync = openSync()
+  '@babel/plugin-proposal-explicit-resource-management',
+  // export module from 语法
+  '@babel/plugin-proposal-export-default-from',
+  // obj::func(val) 语法
+  '@babel/plugin-proposal-function-bind',
+  // 管道函数
+  ['@babel/plugin-proposal-pipeline-operator', { proposal: 'hack', topicToken: '#' }],
+  // var e = throw new Error(err) 语法
+  '@babel/plugin-proposal-throw-expressions',
+  // import json from './foo.json' with { type: 'json' }
+  ['@babel/plugin-syntax-import-assertions', { deprecatedAssertSyntax: true }]
 ];
 
 export default defaultPlugins;
