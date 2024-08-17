b1d25b2925ca5f047fa3d072aa45e859865ae0f9
段昊辰 <duanhaochen@126.com>
2024-08-17 20:41:04 +0800

    refactor: 添加一个vue格式的demo

---
+++ src/layouts/Footer/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ant-layout-footer :class="$style.footer">
+    Copy Right
+  </ant-layout-footer>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+  import { Layout } from 'ant-design-vue';
+
+  export default defineComponent({
+    components: {
+      'ant-layout-footer': Layout.Footer
+    }
+  });
+</script>
+
+<style lang="scss" module>
+  .footer {
+    padding: 0 !important;
+    height: 30px;
+    line-height: 30px;
+    background-color: #ddd;
+    text-align: center;
+  }
+</style>
